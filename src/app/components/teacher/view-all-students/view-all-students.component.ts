c761a2cba72330d058a6b15a30bd4e3b37946af7
shapnesh singh tiwari <shapnesht@gmail.com>
2023-03-24 12:35:05 +0530

    created add class functionality for teachers

---
+++ src/app/components/teacher/view-all-students/view-all-students.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-students',
+  templateUrl: './view-all-students.component.html',
+  styleUrls: ['./view-all-students.component.scss']
+})
+export class ViewAllStudentsComponent {
+
+}
